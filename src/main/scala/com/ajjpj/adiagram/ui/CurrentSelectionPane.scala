fdbc5929b2dc98ace5bf6360d709a2b0b974e1b6
Arno Haase <arno.haase@haase-consulting.com>
2013-07-31 19:52:38 +0200

    minor update

--- src/main/scala/com/ajjpj/adiagram/ui/CurrentSelectionPane.scala
+++ src/main/scala/com/ajjpj/adiagram/ui/CurrentSelectionPane.scala
@@ -1,22 +1,31 @@
 package com.ajjpj.adiagram.ui
 
 import javafx.scene.layout.Pane
-import javafx.scene.control.Label
-import com.ajjpj.adiagram.model.AShapeSpec
+import com.ajjpj.adiagram.model.{ALineSpec, ATextSpec, ABoxSpec, AShapeSpec}
 import com.ajjpj.adiagram.ui.fw.JavaFxHelper
 
 /**
  * @author arno
  */
 class CurrentSelectionPane(selections: SelectionTracker) extends Pane {
-  val label = new Label ("... selection ...") //TODO '<no selection>'
-  getChildren().addAll(label)
-
   selections.selectionChangeListeners += onSelectionChanged
+
+  initForCurrentSelection()
+
+  private def initForCurrentSelection() {
+    getChildren.clear()
+
+    selections.selectedShapes.toList match {
+      case List(box: ABoxSpec) => println("box")
+      case List(box: ALineSpec) => println("line")
+      case List(box: ATextSpec) => println("text")
+      case _ => println ("TODO")
+    }
+  }
 
 
   private def onSelectionChanged(oldSel: Traversable[AShapeSpec], newSel: Traversable[AShapeSpec]) {
-    label.setText(newSel.toString)
+    initForCurrentSelection()
 
     if(! newSel.isEmpty) {
       JavaFxHelper.expandAccordionPaneFor(this)
