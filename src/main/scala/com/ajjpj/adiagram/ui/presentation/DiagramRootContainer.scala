e0f8acd190c2c6952bf82c4c6dc58b27e9fed255
Arno Haase <arno.haase@haase-consulting.com>
2013-08-11 19:09:58 +0200

    cleanup

--- src/main/scala/com/ajjpj/adiagram/ui/presentation/DiagramRootContainer.scala
+++ src/main/scala/com/ajjpj/adiagram/ui/presentation/DiagramRootContainer.scala
@@ -1,12 +1,8 @@
 package com.ajjpj.adiagram.ui.presentation
 
-import javafx.scene.Node
 import javafx.collections.FXCollections
-import java.util.Comparator
-import javafx.scene.layout.{StackPane, Pane}
+import javafx.scene.layout.Pane
 import com.ajjpj.adiagram.ui.fw.Digest
-import javafx.scene.input.MouseEvent
-import javafx.event.EventHandler
 
 /**
  * @author arno
@@ -33,22 +29,8 @@ class DiagramRootContainer(implicit digest: Digest) extends Pane {
     case None =>
   }
 
-  val byZComparator = new Comparator[Node] {
-    def compare(o1: Node, o2: Node): Int = (o1, o2) match {
-      case (n1: ZOrdered, n2: ZOrdered) =>
-        (n1.z - n2.z) match {
-          case 0 => System.identityHashCode(o1) - System.identityHashCode(o2)
-          case n => n
-        }
-      case (n1: ZOrdered, _) => 1
-      case (_, n2: ZOrdered) => -1
-      case _ =>
-        System.identityHashCode(o1) - System.identityHashCode(o2)
-    }
-  }
-
   private def sortByZ() {
-    FXCollections.sort(getChildren, byZComparator)
+    FXCollections.sort(getChildren, ByZComparator)
   }
 }
 
